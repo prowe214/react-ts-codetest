0f7be74c3e03f84ebe4d5038c7e6b210b18f6f63
Paul Rowe <paul.rowe@method.com>
2024-05-23 08:00:13 -0600

    init

---
+++ INSTRUCTIONS.md
@@ -0,0 +1,15 @@
+# Instructions for Code Exercise
+
+We will create a **Random Number Generator**.
+
+1. Open new branch
+2. The UI will include:
+   - One button
+   - Two separate numbers
+   - Ignore styles for this exercise
+3. Behavior:
+   - Pressing the button generates 2 random numbers (0-3)
+     - Code for Random Number: `Math.floor(Math.random() * 4)`
+   - The numbers are displayed in the UI
+   - If the numbers match, show the word “MATCH!” in the UI
+4. When complete, commit and push (the push command will fail, its okay, ignore)
---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import reactLogo from "./assets/react.svg";
+import methodLogo from "./assets/method.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo" alt="Method logo" />
+      </div>
+      <div>
+        <img src={methodLogo} className="logo" alt="Method logo" />
+      </div>
+      <h1>React+TS Code Exercise</h1>
+      <div className="card">
+        <p>
+          Open <code>INSTRUCTIONS.md</code> in this repository for your
+          requirements.
+        </p>
+        <p>
+          This repository is a starting point for a React+TypeScript code
+          exercise.
+        </p>
+        <p>
+          When you are ready to start the exercise,{" "}
+          <strong>delete all content from this component</strong> and start
+          coding!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
